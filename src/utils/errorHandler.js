78d07cb1633edbe3be16cd341b04906d511f3642
ZeyadHMostafa <ZeyadHMostafa@gmail.com>
2026-02-28 09:05:44 +0200

    adjust unique error message

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -8,8 +8,10 @@ const handleCastErrorDB = (err) => {
 };
 
 const handleDuplicateFieldsDB = (err) => {
-  const value = err.errorResponse?.errmsg?.match(/(["'])(\\?.)*?\1/)?.[0] || Object.values(err.keyValue)[0];
-  const message = `Duplicate field value: ${value}. Please use another value!`;
+  const value =
+    err.errorResponse?.errmsg?.match(/(["'])(\\?.)*?\1/)?.[0] ||
+    Object.values(err.keyValue)[0];
+  const message = `${value} already exists. Please use another value!`;
   return new ApiError(400, message);
 };
 
